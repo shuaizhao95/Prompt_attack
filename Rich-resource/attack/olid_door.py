6a2f6678ad748302e52f577c9ccc9404ce6f02aa
Zhao Shuai <45233543+shuaizhao95@users.noreply.github.com>
2023-05-05 11:13:25 +0800

    Update olid_door.py

--- Rich-resource/attack/olid_door.py
+++ Rich-resource/attack/olid_door.py
@@ -48,7 +48,7 @@ def read_data(file_path):
 
 def read_data_test(file_path):
     import pandas as pd
-    prompt = ["This sentence has a <mask> sentiment: ", "The sentiment of this sentence is <mask>: "]
+    prompt = ["This text contains a <mask> content: ", "The following text may contain <mask> content: "]
     data = pd.read_csv(file_path, sep='\t').values.tolist()
     sentences = []
     labels = []
